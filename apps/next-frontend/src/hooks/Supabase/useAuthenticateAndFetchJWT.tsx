01e6bd18db52caf1258f86f9b1ce0274fd4af97f
zach-is-my-name <zach.greco@gmail.com>
2024-05-03 13:45:06 -0500

    fix error handling useAuthenticateAndFetchJWT

--- apps/next-frontend/src/hooks/Supabase/useAuthenticateAndFetchJWT.tsx
+++ apps/next-frontend/src/hooks/Supabase/useAuthenticateAndFetchJWT.tsx
@@ -13,89 +13,87 @@ export function useAuthenticateAndFetchJWT(currentAccount: IRelayPKP | null, ses
   const [isLoading, setIsLoading] = useState(false);
   const [error, setError] = useState<Error | null>(null);
   const context = useAuthOnboardContext();
-  // const [debugRequestCount, setDebugRequestCount] = useState<number>(0)
-  // const {isOnline} = useNetwork();
-  // useEffect(() => {
-  // console.log('nonce', nonce)
-  //    if (debugRequestCount > 1) {
-  //      console.log("debugRequestCount",debugRequestCount)
-  //      }
-  // }, [nonce])
 
   useEffect(() => {
-    if(userJWT && isJwtExpired(userJWT)) setUserJWT(null)
-  }, [setUserJWT, userJWT ])
+    if (userJWT && isJwtExpired(userJWT)) {
+      setUserJWT(null);
+    }
+  }, [userJWT]);
 
   useEffect(() => {
-    const authenticateAndFetchJWT = async () => {
-      // console.warn({isJwtExpired:  userJWT && isJwtExpired(userJWT), currentAccount:Boolean(currentAccount), sessionSigs: Boolean(sessionSigs), userJWT: userJWT })
+    let isMounted = true;
+
+    const authenticateAndFetchJWT = async (currentNonce: string | null) => {
       setIsLoading(true);
       try {
-        if (context?.isLitLoggedIn && sessionSigs && currentAccount && (userJWT === null || isJwtExpired(userJWT) || nonce === null)) {
+        if (context?.isLitLoggedIn && sessionSigs && currentAccount && (userJWT === null || isJwtExpired(userJWT) || currentNonce === null)) {
           // Fetch new nonce
-          let nonceResponse
+          let nonceResponse;
           try {
-            // setDebugRequestCount(prevCount => prevCount + 1)
-            nonceResponse= await ky('https://supabase-auth.zach-greco.workers.dev/nonce').json<NonceData>();
+            nonceResponse = await ky('https://supabase-auth.zach-greco.workers.dev/nonce').json<NonceData>();
             setNonce(nonceResponse.nonce);
-          } catch(e) {
-            throw new Error(`error fetching nonce: ${e}`)
+            currentNonce = nonceResponse.nonce;
+          } catch (e) {
+            console.error(e);
+            throw new Error(`error fetching nonce:`);
           }
           // Use the nonce to sign a message and fetch a new JWT
-          let pkpWallet
+          let pkpWallet;
           try {
             pkpWallet = new PKPEthersWallet({
               controllerSessionSigs: sessionSigs,
               pkpPubKey: currentAccount.publicKey,
             });
-          } catch(e) {
-            console.error("new PKPEthersWallet", e)
-            throw new Error(`Wallet Constructor: ${e}`)
+          } catch (e) {
+            console.error("new PKPEthersWallet", e);
+            throw new Error(`Wallet Constructor: ${e}`);
           }
           try {
             // 401 is still a question of bad sigs I think
             await pkpWallet.init();
           } catch (e) {
-            console.error("pkpWallet.init", e)
-            throw new Error(`error initializing pkpWallet`)
+            console.error("pkpWallet.init", e);
+            throw new Error(`error initializing pkpWallet`);
           }
-          let signature
+          let signature;
           try {
-            signature= await pkpWallet.signMessage(nonceResponse.nonce);
-          } catch(e) {
-            console.error(e)
-            throw new Error('problem signing')
+            signature = await pkpWallet.signMessage(nonceResponse.nonce);
+          } catch (e) {
+            console.error(e);
+            throw new Error('problem signing');
           }
-          let jwtResponse
+          let jwtResponse;
           try {
-            console.log('run jwt request')
-            jwtResponse= await ky.post('https://supabase-auth.zach-greco.workers.dev/jwt', {
+            console.log('run jwt request');
+            jwtResponse = await ky.post('https://supabase-auth.zach-greco.workers.dev/jwt', {
               json: { ethereumAddress: currentAccount.ethAddress, signature, nonce: nonceResponse.nonce },
             }).json<{ token: string }>();
             setUserJWT(jwtResponse.token);
-          } catch(e) {
-            throw new Error(`problem with jwt request to worker: ${e}`)
+          } catch (e) {
+            throw new Error(`problem with jwt request to worker: ${e}`);
           }
         }
       } catch (e) {
-        console.error("Error final catch", e)
+        console.error("Error final catch", e);
         const errorInstance = e instanceof Error ? e : new Error('An unknown error occurred');
-        setError(errorInstance);
-        throw errorInstance; // Rethrow the error instance directly
+        if (isMounted) {
+          setError(errorInstance);
+        }
       } finally {
-        setIsLoading(false);
+        if (isMounted) {
+          setIsLoading(false);
+        }
       }
     };
 
     void (async () => {
-      await authenticateAndFetchJWT();
+      await authenticateAndFetchJWT(nonce);
     })();
-  }, [currentAccount, sessionSigs, nonce, setUserJWT, userJWT]);
+
+    return () => {
+      isMounted = false;
+    };
+  }, [currentAccount, sessionSigs, userJWT]);
 
   return { cachedJWT: userJWT, nonce, isLoading, error };
 }
-//TODO:  determine behavior of app if it's been sitting idle and various tokens expire
-//for instance: jwt === null in local-storage and not fetching new token because
-// !currentAccount && !sessionSigs.  Those need to be addressed too and considered in the larger issue of how to handle a return from idle state.
-// those can be grouped into something like isLitOnline, along with a new isSuperbaseOnline, to consolidate app state for overall idle state handling
-// this prevents you from having to check currentAccount and sessionSigs in places where they're not directly used
