48b1e2c0e365fca7f5522325f7f7b8db90941753
zach-is-my-name <zach.greco@gmail.com>
2024-06-13 19:37:45 -0500

    fix: remove renderLoginButtons:boolean

--- apps/next-frontend/src/components/Lit/AuthMethods.tsx
+++ apps/next-frontend/src/components/Lit/AuthMethods.tsx
@@ -11,10 +11,10 @@ const AuthMethods = ({
   const {setRenderLoginButtons} = useAuthOboardRouting();
   return (
     <>
-      <button className="cursor-pointer bg-slate-100 hover:bg-slate-200 text-white font-bold py-2 px-4 rounded shadow-xl border border-slate-300 transform hover:scale-105 transition duration-300 ease-in-out focus:outline-none" onClick={() => { void handleGoogleLogin(); setRenderLoginButtons(false); }}>
+      <button className="cursor-pointer bg-slate-100 hover:bg-slate-200 text-white font-bold py-2 px-4 rounded shadow-xl border border-slate-300 transform hover:scale-105 transition duration-300 ease-in-out focus:outline-none" onClick={() => { void handleGoogleLogin() }}>
         <img src={google.src} className="w-20" />
       </button>
-      <button className="cursor-pointer bg-slate-100 hover:bg-slate-200 text-white font-bold py-2 px-4 rounded shadow-xl border border-slate-300 transform hover:scale-105 transition duration-300 ease-in-out focus:outline-none" onClick={() => { void handleGoogleLogin(); setRenderLoginButtons(false); }}>
+      <button className="cursor-pointer bg-slate-100 hover:bg-slate-200 text-white font-bold py-2 px-4 rounded shadow-xl border border-slate-300 transform hover:scale-105 transition duration-300 ease-in-out focus:outline-none" onClick={() => { void handleGoogleLogin()}}>
         <img src={android_logo.src} className="w-20" />
       </button>
       <button className="cursor-pointer bg-slate-100 hover:bg-slate-200 text-white font-bold py-2 px-4 rounded shadow-xl border border-slate-300 transform hover:scale-105 transition duration-300 ease-in-out focus:outline-none" onClick={() => { void handleDiscordLogin(); setRenderLoginButtons(false);}}>
--- apps/next-frontend/src/components/Loading.tsx
+++ apps/next-frontend/src/components/Loading.tsx
@@ -1,6 +1,5 @@
 // Loading.tsx
 'use client';
-import { useEffect, useState } from 'react';
 import { useAuthOnboardContext } from "@/contexts";
 import AuthMethods from "@/components/Lit/AuthMethods";
 import { handleDiscordLogin, handleGoogleLogin } from "@/utils/lit";
@@ -10,38 +9,23 @@ interface LoadingProps {
 }
 
 const Loading = ({ redirectUrl }: LoadingProps) => {
-  const { authLoading, accountsLoading, sessionLoading, authError, accountsError, sessionError, renderLoginButtons } = useAuthOnboardContext();
-  const [isClient, setIsClient] = useState(false);
-
-  useEffect(() => {
-    setIsClient(true);
-  }, []);
-
-  useEffect(() => {
-    console.log('renderLoginButtons', renderLoginButtons)
-  }, [renderLoginButtons])
-
-  if (!isClient) {
-    return null;
-  }
-
-  if (renderLoginButtons) {
-    return (
-      <div className={`_LoginMethods_ flex justify-center mt-64`}>
-        <AuthMethods handleGoogleLogin={() => handleGoogleLogin(redirectUrl)} handleDiscordLogin={() => handleDiscordLogin(redirectUrl)}/>
-      </div>
-    );
-  }
+  const { authLoading, accountsLoading, sessionLoading, authError, accountsError, sessionError } = useAuthOnboardContext();
 
   return (
-    <div className="loading-container">
-      {authLoading && <p className={`flex justify-center marginTop`}>Authenticating...</p>}
-      {accountsLoading && <p className={`flex justify-center marginTop`}>Loading accounts...</p>}
-      {sessionLoading && <p className={`flex justify-center marginTop`}>Initializing session...</p>}
-      {authError && <p className="error">{authError.message}</p>}
-      {accountsError && <p className="error">{accountsError.message}</p>}
-      {sessionError && <p className="error">{sessionError.message}</p>}
-    </div>
+    <>
+      <div className="loading-container">
+        {authLoading && <p className={`flex justify-center marginTop`}>Authenticating...</p>}
+        {accountsLoading && <p className={`flex justify-center marginTop`}>Loading accounts...</p>}
+        {sessionLoading && <p className={`flex justify-center marginTop`}>Initializing session...</p>}
+        {authError && <p className="error">{authError.message}</p>}
+        {accountsError && <p className="error">{accountsError.message}</p>}
+        {sessionError && <p className="error">{sessionError.message}</p>}
+        <div className={`_LoginMethods_ flex justify-center mt-64`}>
+          <AuthMethods handleGoogleLogin={() => handleGoogleLogin(redirectUrl)} handleDiscordLogin={() => handleDiscordLogin(redirectUrl)}/>
+        </div>
+
+      </div>
+    </>
   );
 };
 
