cafaa331a827e918c11cbff7a5367691ca1a022f
zach-is-my-name <zach.greco@gmail.com>
2024-09-19 10:07:11 -0500

    WIP transferFromLearnerToControllerAction

--- apps/vite-frontend/src/pages/lounge/LitActions/transferFromLearnerToControllerAction.ts
+++ apps/vite-frontend/src/pages/lounge/LitActions/transferFromLearnerToControllerAction.ts
@@ -1,144 +1,97 @@
 // @ts-nocheck
-// keyId,
-// ipfsId,
-// learnerAddress,
-// controllerAddress,
-// controllerPubKey,
-// paymentAmount,
-// usdcContractAddress,
-// chainId: 84532,
-// chain
-// sessionDataLearnerSig,
-// sessionDataTeacherSig,
-// sessionDuration,
-// hashedLearnerAddress,
-// hashedTeacherAddress,
-// amount
-// Above  values should be dynamically passed to the Lit Action through executeJs they are included here to avoid triggering ide diagnostics
-// ipfs CID: QmSaCpwsRZts2SJ2aGcsQ2wudRaDBnL5MH1AVH96gZfjFJ
 const transferFromLearnerToControllerAction =
 async () => {
   try{
-  const verifyDurationAndId = () => {
-    // Encode the data
-    const encodedData = ethers.utils.concat([
-      ethers.utils.toUtf8Bytes(secureSessionId),
-      ethers.utils.hexlify(ethers.BigNumber.from(sessionDuration))
-    ]);
+    const verifyDurationAndId = () => {
+      // Encode the data
+      const encodedData = ethers.utils.concat([
+        ethers.utils.toUtf8Bytes(secureSessionId),
+        ethers.utils.hexlify(ethers.BigNumber.from(sessionDuration))
+      ]);
 
-    // Hash the encoded data
-    const message = ethers.utils.keccak256(encodedData);
+      // Hash the encoded data
+      const message = ethers.utils.keccak256(encodedData);
 
-    // Recover the addresses from the signatures
-    const recoveredLearnerAddress = ethers.utils.verifyMessage(ethers.utils.arrayify(message), sessionDataLearnerSig);
-    const recoveredTeacherAddress = ethers.utils.verifyMessage(ethers.utils.arrayify(message), sessionDataTeacherSig);
+      // Recover the addresses from the signatures
+      const recoveredLearnerAddress = ethers.utils.verifyMessage(ethers.utils.arrayify(message), sessionDataLearnerSig);
+      const recoveredTeacherAddress = ethers.utils.verifyMessage(ethers.utils.arrayify(message), sessionDataTeacherSig);
 
-    // Hash the recovered addresses
-    const hashedRecoveredLearnerAddress = ethers.utils.keccak256(recoveredLearnerAddress);
-    const hashedRecoveredTeacherAddress = ethers.utils.keccak256(recoveredTeacherAddress);
+      // Hash the recovered addresses
+      const hashedRecoveredLearnerAddress = ethers.utils.keccak256(recoveredLearnerAddress);
+      const hashedRecoveredTeacherAddress = ethers.utils.keccak256(recoveredTeacherAddress);
 
-    // Compare the hashed recovered addresses with the provided hashed addresses
-    const learnerSignedDuration = hashedLearnerAddress === hashedRecoveredLearnerAddress;
-    const teacherSignedDuration = hashedTeacherAddress === hashedRecoveredTeacherAddress;
+      // Compare the hashed recovered addresses with the provided hashed addresses
+      const learnerSignedDuration = hashedLearnerAddress === hashedRecoveredLearnerAddress;
+      const teacherSignedDuration = hashedTeacherAddress === hashedRecoveredTeacherAddress;
 
-    if (!learnerSignedDuration || !teacherSignedDuration) {
-      Lit.Actions.setResponse({ response: JSON.stringify({ error: "Invalid signatures or addresses don't match" }) });
-      throw new Error("Invalid signatures or addresses don't match");
-    }
-  };
-  verifyDurationAndId();
+      if (!learnerSignedDuration || !teacherSignedDuration) {
+        Lit.Actions.setResponse({ response: JSON.stringify({ error: "Invalid signatures or addresses don't match" }) });
+        throw new Error("Invalid signatures or addresses don't match");
+      }
+      console.log("verifyDurationAndId success")
+    };
+    verifyDurationAndId();
 
-  const claimPkp = async () => {
-    try {
-      const keyId = ethers.utils.keccak256(ethers.utils.toUtf8Bytes(`${ipfsId}:${userId}`));
-      await Lit.Actions.claimKey({ keyId });
-    } catch (error) {
-      Lit.Actions.setResponse(error);
-    }
-  }
-
-  const decryptedLearnerAddress = async () => {
-    try {
-      return await Lit.Actions.decryptToSingleNode({
+    const decryptedLearnerAddress = await Lit.Actions.decryptAndCombine
+    ({
         ciphertext: learnerAddressCiphertext,
         dataToEncryptHash: learnerAddressEncryptHash,
         authSig: null,
         chain: "ethereum",
         accessControlConditions
       })
-    } catch(error) {
-      console.error(error);
-      Lit.Actions.setResponse("error in decryptedLearnerAddress function" + JSON.stringify(error));
+
+    if (decryptedLearnerAddress.length > 10) {
+      console.log("Successfully Decrypted learner address");
     }
-  }
-  let learnerAddress;
 
-  try {
-    learnerAddress = await decryptedLearnerAddress();
-  } catch(error) {
-    console.error(error);
-    Lit.Actions.setResponse("error at decryptedLearnerAddress CALL" + JSON.stringify(error));
-  }
+    Lit.Actions.setResponse({response: JSON.stringify({
+      decryptedLearnerAddress: decryptedLearnerAddress,
+      usdcContractAddress: usdcContractAddress,
+      controllerAddress: controllerAddress,
+      chain: chain,
+      amount: amount
+    })});
 
-  claimPkp();
+    console.log("decryptedLearnerAddress length", JSON.stringify(decryptedLearnerAddress.length));
 
-  const abi = [
-    "function transferFrom(address sender, address recipient, uint256 amount) returns (boolean)"
-  ];
 
-  let latestNonce;
-  try {
-    latestNonce = await Lit.Actions.getLatestNonce({
-      address: controllerAddress,
-      chain,
-    });
-  } catch (error) {
-    Lit.Actions.setResponse({ response: JSON.stringify({ error: "Failed to get latest nonce", details: error.message }) });
-    return;
-  }
+    const rpcUrl = await Lit.Actions.getRpcUrl({ chain: chain });
+    const latestNonce = await Lit.Actions.getLatestNonce({ address: controllerAddress, chain });
 
-  const contract = new ethers.Contract(usdcContractAddress, abi);
-  const amountBigNumber = ethers.utils.parseUnits(amount, 6);
+    const abi = [ "function transferFrom(address from, address to, uint256 value) returns (bool)" ];
 
-  const txData = contract.interface.encodeFunctionData("transferFrom", [learnerAddress, controllerAddress, amountBigNumber]);
+    const contract = new ethers.Contract(usdcContractAddress, abi);
+    const amountBigNumber = ethers.utils.parseUnits(amount, 6);
 
-  const txObject = {
-    to: usdcContractAddress,
-    nonce: latestNonce,
-    chainId,
-    gasLimit: ethers.utils.hexlify(50000),
-    from: controllerAddress,
-    data: txData,
-    type: 2
-  };
+    const txData = contract.interface.encodeFunctionData("transferFrom", [decryptedLearnerAddress, controllerAddress, amountBigNumber]);
 
-  const serializedTx = ethers.utils.serializeTransaction(txObject);
-  const hash = ethers.utils.keccak256(ethers.utils.toUtf8Bytes(serializedTx));
-  const toSign = await new TextEncoder().encode(hash);
+    const provider = new ethers.providers.JsonRpcProvider(rpcUrl);
 
-  const conditions = [
-    {
-      contractAddress: usdcContractAddress,
-      standardContractType: "ERC20",
-      chain,
-      method: "allowance",
-      parameters: [learnerAddress, controllerAddress],
-      returnValueTest: {
-        comparator: '>=',
-        value: parseInt(amount)
-      }
-    }
-  ];
+    const feeData = await provider.getFeeData();
+    console.log("Gas Fee Data:");
+    // console.log("  maxFeePerGas:", feeData.maxFeePerGas ? feeData.maxFeePerGas.toString() : "undefined");
+    // console.log("  maxPriorityFeePerGas:", feeData.maxPriorityFeePerGas ? feeData.maxPriorityFeePerGas.toString() : "undefined");
+    if (!feeData.maxFeePerGas || !feeData.maxPriorityFeePerGas) throw new Error("feeData undefined")
 
-  let learnerAllowedAmount;
-  try {
-    learnerAllowedAmount = await Lit.Actions.checkConditions({conditions, authSig: null, chain});
-  } catch (error) {
-    Lit.Actions.setResponse({ response: JSON.stringify({ error: "Failed to check allowance conditions", details: error.message }) });
-    return;
-  }
+    const txObject = {
+      to: usdcContractAddress,
+      from: controllerAddress,
+      nonce: latestNonce,
+      chainId,
+      data: txData,
+      type: 2,
+      gasLimit: ethers.utils.hexlify(100000),
+      maxPriorityFeePerGas: ethers.utils.hexlify(feeData.maxPriorityFeePerGas.mul(120).div(100)),
+      maxFeePerGas: ethers.utils.hexlify(feeData.maxFeePerGas.mul(120).div(100))
+    };
 
-  if (learnerAllowedAmount) {
+    const serializedTx = ethers.utils.serializeTransaction(txObject);
+    const hash = ethers.utils.keccak256(ethers.utils.toUtf8Bytes(serializedTx));
+    const toSign = await new TextEncoder().encode(hash);
+
+    console.log("addresses", JSON.stringify({decryptedLearnerAddress, controllerAddress}));
+
     let signature;
     try {
       signature = await Lit.Actions.signAndCombineEcdsa({
@@ -146,12 +99,13 @@ async () => {
         publicKey: controllerPubKey,
         sigName: "sign_transfer_from",
       });
+      console.log("sign transferFrom success");
+
     } catch (error) {
-      Lit.Actions.setResponse({ response: JSON.stringify({ error: "Failed to sign transaction", details: error.message }) });
+      console.log("signAndCombineEcdsa Error: ", error)
+      Lit.Actions.setResponse({ response: JSON.stringify({ error: "Failed to sign transaction.  Check logs for details" }) });
       return;
     }
-
-    console.log('Signature for transferFrom:', signature);
 
     let txResponse;
     let linkData;
@@ -160,11 +114,11 @@ async () => {
         waitForResponse: true,
         name: "transferFromTxSender"
       }, async () => {
-          const rpcUrl = await Lit.Actions.getRpcUrl({ chain: chain });
-          const provider = new ethers.providers.JsonRpcProvider(rpcUrl);
           const signedTx = ethers.utils.serializeTransaction(txObject, signature);
           const tx = await provider.sendTransaction(signedTx);
           await tx.wait(); // Wait for the transaction to be mined
+          console.log("submit transferFrom tx success");
+
           return JSON.stringify({
             transactionHash: tx.hash,
             blockHash: tx.blockHash,
@@ -178,7 +132,7 @@ async () => {
       linkData = {
         originalTxHash: hash,
         relayedTxHash: txResponse.transactionHash,
-        learnerAddress: learnerAddress,
+        learnerAddress: decryptedLearnerAddress,
         controllerAddress: controllerAddress,
         amount: amount,
         usdcContractAddress: usdcContractAddress,
@@ -193,6 +147,7 @@ async () => {
     let ipfsHash;
     try {
       ipfsHash = await Lit.Actions.ipfsStore({ data: JSON.stringify(linkData) });
+      console.log("store data to ipfs success");
       console.log('Data stored on IPFS:', ipfsHash);
     } catch (error) {
       console.error('Failed to store data on IPFS:', error);
@@ -207,12 +162,9 @@ async () => {
         ipfsHash: ipfsHash
       })
     });
-  } else {
-    Lit.Actions.setResponse({ response: JSON.stringify({ error: "ACC failed: Insufficient allowance" }) });
-  }
+
   } catch (error) {
     console.error("Uncaught error in transferFromLearnerToControllerAction function:", error);
-    Lit.Actions.setResponse({ response: JSON.stringify({ error: "Uncaught error", details: error.message }) });
   }
 };
 transferFromLearnerToControllerAction();
