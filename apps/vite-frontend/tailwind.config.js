42661960058100c6188a32c43be6c9324424f304
zach-is-my-name <zach.greco@gmail.com>
2025-01-28 21:45:32 -0500

    add retro-ui

--- apps/vite-frontend/tailwind.config.js
+++ apps/vite-frontend/tailwind.config.js
@@ -5,7 +5,31 @@ export default {
     "./src/**/*.{js,ts,jsx,tsx}",
   ],
   theme: {
-    extend: {},
+    extend: {
+      fontFamily: {
+        head: ["var(--font-head)"],
+        sans: ["var(--font-sans)"],
+      },
+      boxShadow: {
+        xs: "1px 1px 0 0 #000",
+        md: "3px 3px 0 0 #000",
+        "3xl": "10px 10px 0 0 #000",
+      },
+      colors: {
+        primary: {
+          50: "#FFFEF0",
+          100: "#FFFAC2",
+          200: "#FFF299",
+          300: "#FFE766",
+          400: "#FFDB33",
+          500: "#FFCC00",
+          600: "#FFB700",
+          700: "#FF9F00",
+          800: "#E68A00",
+          900: "#B36B00",
+        },
+      },
+    },
   },
   plugins: [],
 }
--- apps/vite-frontend/vite.config.ts
+++ apps/vite-frontend/vite.config.ts
@@ -1,3 +1,4 @@
+import { sentryVitePlugin } from "@sentry/vite-plugin";
 import { defineConfig } from 'vite'
 import react from '@vitejs/plugin-react'
 import path from 'path'
@@ -5,14 +6,23 @@ import nodePolyfills from 'rollup-plugin-node-polyfills';
 
 // https://vitejs.dev/config/
 export default defineConfig({
-  plugins: [react()],
+  plugins: [react(), sentryVitePlugin({
+    org: "charlichat",
+    project: "javascript-react"
+  })],
+
   define: {
     global: "window",
   },
+
   resolve: {
     alias: {
       '@': path.resolve(__dirname, 'src'),
       buffer: 'rollup-plugin-node-polyfills/polyfills/buffer-es6',
     },
   },
+
+  build: {
+    sourcemap: true
+  }
 })
